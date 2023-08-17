4c3409d89123e74e0ed8b8d1a921ea5e17ea159e
trantthien2503 <49374309+trantthien2503@users.noreply.github.com>
2023-08-17 14:31:50 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
 from flask_cors import CORS
+from flask_restful import Resource, Api
 
 
 app = Flask(__name__)
+api = Api(app)
 CORS(app)
 
 app.config.from_object('config')
